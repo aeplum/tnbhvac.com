37dbdcb711105158f531d57a46c3ccb6311f0c5f
aeplum <memorixx@yonsei.ac.kr>
2023-10-18 10:47:43 +0900

    tnb commit

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title> Solid | Next.js Template for Startup and SaaS</title>
+      <title> T&B AIR DUCT HVAC LLCS</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Built with Next.js and TypeScript" />
+      <meta name="description" content="DUCT manufacturing and installation company for batteries and semiconductors" />
       <link rel="icon" href="/images/favicon.ico" />
     </>
   );
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -18,38 +18,23 @@ const Hero = () => {
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
             <div className=" md:w-1/2">
               <h4 className="text-black dark:text-white text-lg font-medium mb-4.5">
-                🔥 Solid - A Complete SaaS Web Template
+                🔥 TnB Air Duct HVAC LLC
               </h4>
               <h1 className="text-black dark:text-white text-3xl xl:text-hero font-bold mb-5 pr-16 ">
-              Free Next.js Template for 
+              TnB Air Duct HVAC LLC
                 <span className="inline-block relative before:absolute before:bottom-2.5 before:left-0 before:w-full before:h-3 before:bg-titlebg dark:before:bg-titlebgdark before:-z-1 ml-2.5 ">SaaS
                 </span>
               </h1>
               <p>
-Solid Pro - Packed with all the key integrations you need for swift SaaS startup launch, including - Auth, Database, Sanity Blog, Essential Components, Pages and More. Built-winth - Next.js 13, React 18 and TypeScript.
+              T&B AIR DUCT HVAC LLC are a professional manufacturing and installation company specializing in air conditioning and ventilation, and particularly in duct production for semiconductor manufacturing plants and battery manufacturing plants.
+We provides services that meet customer requirements through strict quality control and 
+confidence.
               </p>
 
               <div className="mt-10">
-                <form onSubmit={handleSubmit}>
-                  <div className="flex flex-wrap gap-5">
-                    <input
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      type="text"
-                      placeholder="Enter your email address"
-                      className="dark:bg-black border border-stroke dark:border-strokedark shadow-solid-2 dark:shadow-none rounded-full focus:outline-none focus:border-primary dark:focus:border-primary py-2.5 px-6"
-                    />
-                    <button
-                      aria-label="signup to newsletter"
-                      className="flex bg-black hover:bg-blackho dark:bg-btndark text-white rounded-full ease-in-out duration-300 px-7.5 py-2.5"
-                    >
-                      Get Started
-                    </button>
-                  </div>
-                </form>
 
                 <p className="text-black dark:text-white mt-5">
-                  Try for free no credit card required.
+                T&B AIR DUCT HVAC LLC (U.S.A) Calhoon Factory
                 </p>
               </div>
             </div>
